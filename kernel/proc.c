fd3a01e3f9c523bd7e35178643dc06b1be1593a4
XR <s230101165@stu.cqupt.edu.cn>
2025-06-19 21:13:53 +0800

    完成第二个作业

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,7 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->kama_syscall_trace = 0;
   return p;
 }
 
@@ -296,6 +297,8 @@ fork(void)
   np->state = RUNNABLE;
 
   release(&np->lock);
+
+  np->kama_syscall_trace = p->kama_syscall_trace;
 
   return pid;
 }
@@ -692,4 +695,15 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// 统计处于活动状态的进程
+void
+kama_procnum(uint64* dst) {
+    *dst = 0;
+    struct proc* p;
+    for (p = proc;p < &proc[NPROC];p++) {
+        if (p->state != UNUSED)
+            (*dst)++;
+    }
 }
